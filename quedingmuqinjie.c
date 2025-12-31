30b20cbdac4dcdb85036e73a91e4a158c4908e04
syyuan0616-byte <syyuan0616@gmail.com>
2025-12-31 11:04:57 +0800

    确定母亲节

---
+++ quedingmuqinjie.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int year,month=5,date=7,days=0,weekday,mom;
+	scanf("%d",&year);
+	for(int i=1900;i<=year-1;i++)  
+    {  
+        if(i%400==0||(i%4==0&&i%100!=0)) days+=366;  
+        else days+=365;  
+    }  
+    for(int i=1;i<=month-1;i++)  
+    {  
+        if(i==1||i==3||i==5||i==7||i==8||i==10||i==12) days+=31;  
+        else if((i==2)&&(year%400==0||(year%4==0&&year%100!=0))) days+=29;  
+        else if(i==2) days+=28;  
+        else days+=30;  
+    }  
+    days+=date;  
+    weekday=days%7;
+	mom=date+7-weekday;
+	printf("%d\n",mom);
+    
+}
