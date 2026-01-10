f7adbab01321f3f1874695b1eff43af1d30f79b1
syyuan0616-byte <syyuan0616@gmail.com>
2026-01-10 18:00:52 +0800

    另一道链表题，有关于学生成绩排序的

---
+++ lianbiao.c
@@ -0,0 +1,53 @@
+/* PRESET CODE BEGIN - NEVER TOUCH CODE BELOW */  
+ 
+#include <stdio.h>  
+#include <stdlib.h>  
+  
+struct Node {  
+    char *name;  
+    int  score;  
+    struct Node *next;  
+};  
+  
+struct Node *  connect(struct Node *x, struct Node *y, struct Node *z);  
+struct Node *  connect(struct Node *x, struct Node *y, struct Node *z)
+{
+	struct Node *other;
+	if(x->score<y->score) 
+	{
+		other=x;
+		x=y;
+		y=other;
+	}
+	if(y->score<z->score) 
+	{
+		other=y;
+		y=z;
+		z=other;
+	}
+	if(x->score<y->score) 
+	{
+		other=x;
+		x=y;
+		y=other;
+	}
+	x->next=y;
+	y->next=z;
+	return x;
+}
+int main(int argc, char *argv[]) {      
+     struct Node a, b, c, *p;  
+     a.name = "LiPing";  
+     b.name = "LiuHai";  
+     c.name = "FengYun";   
+     scanf("%d,%d,%d", &a.score, &b.score, &c.score);  
+     p= connect(&a,&b,&c);  
+     printf("%s-%d\n",p->name,p->score);  
+     p=p->next;  
+     printf("%s-%d\n",p->name,p->score);  
+     p=p->next;  
+     printf("%s-%d\n",p->name,p->score);  
+     return 0;  
+}  
+  
+/* PRESET CODE END - NEVER TOUCH CODE ABOVE */  
